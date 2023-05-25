ef9174b4b943d3ec5254c6ed7533f1d426ff91a3
CodeAkio <siqueira_2004@hotmail.com>
2023-05-24 22:52:30 -0300

    chore: update deps

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Rockelivery.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:gettext] ++ Mix.compilers(),
+      compilers: [] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps(),
@@ -52,7 +52,7 @@ defmodule Rockelivery.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:pbkdf2_elixir, "~> 1.4"},
       {:excoveralls, "~> 0.10", only: :test},
       {:ex_machina, "~> 2.7.0"}
